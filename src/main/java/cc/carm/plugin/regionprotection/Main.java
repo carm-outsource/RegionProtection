1c730f9ca31f2aa4e848e5f2a36c4c468ffd6113
carm <Karmun@foxmail.com>
2022-02-16 23:32:04 +0800

    初始化项目代码

---
+++ src/main/java/cc/carm/plugin/regionprotection/Main.java
@@ -0,0 +1,12 @@
+package cc.carm.plugin.regionprotection;
+
+import cc.carm.lib.easyplugin.EasyPlugin;
+
+public class Main extends EasyPlugin {
+
+    @Override
+    public boolean initialize() {
+        return false;
+    }
+
+}
